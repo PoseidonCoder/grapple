26aad27b6abbc51d9b6ffd15358d2ebfe0ce7429
Lukas <lukasdobbles@gmail.com>
2021-03-12 21:44:37 -0800

    ✨ simple movements

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,36 @@
 kaboom.import();
+
+const speed = 2;
+
 init({
 	fullscreen: true,
 	scale: 2,
 });
 
 scene('main', () => {
-	add([text('hello world'), pos(100, 100)]);
+	//loadSprite("froggy", "player);
+
+	const player = add([rect(100, 100), pos(100, 100)]);
+
+	// console.log(player);
+
+	keyDown('w', () => {
+		player.pos.y -= speed;
+	});
+
+	keyDown('s', () => {
+		player.pos.y += speed;
+	});
+
+	keyDown('a', () => {
+		player.pos.x -= speed;
+	});
+
+	keyDown('d', () => {
+		player.pos.x += speed;
+	});
+
+	add([player]);
 });
 
 start('main');
