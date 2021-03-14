685ead689a420c93d2f0a00e3b069bb1aaa88da7
Lukas <computorgenius101@gmail.com>
2021-03-13 17:04:41 -0800

    🎨 converted scene to class

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,4 +1,63 @@
-const gameScene = new Phaser.Scene('game');
+class gameScene extends Phaser.Scene {
+	constructor() {
+		super();
+		this.speed = 3;
+	}
+
+	preload() {
+		this.load.image('player', 'assets/images/player.png');
+	}
+
+	create() {
+		this.player = this.physics.add.sprite(100, 100, 'player');
+		this.player.setScale(3);
+
+		this.upKey = this.input.keyboard.addKey(
+			Phaser.Input.Keyboard.KeyCodes.UP
+		);
+		this.downKey = this.input.keyboard.addKey(
+			Phaser.Input.Keyboard.KeyCodes.DOWN
+		);
+		this.leftKey = this.input.keyboard.addKey(
+			Phaser.Input.Keyboard.KeyCodes.LEFT
+		);
+		this.rightKey = this.input.keyboard.addKey(
+			Phaser.Input.Keyboard.KeyCodes.RIGHT
+		);
+
+		this.input.on('pointermove', (event) => {
+			const angle =
+				Phaser.Math.RAD_TO_DEG * // converts the radians to degress
+				Phaser.Math.Angle.Between(
+					// calculates the angle in radians
+					this.player.x,
+					this.player.y,
+					event.x,
+					event.y
+				);
+			this.player.setAngle(angle);
+		});
+	}
+
+	update() {
+		if (this.upKey.isDown) {
+			this.player.y -= this.speed;
+		}
+
+		if (this.downKey.isDown) {
+			this.player.y += this.speed;
+		}
+
+		if (this.leftKey.isDown) {
+			this.player.x -= this.speed;
+		}
+
+		if (this.rightKey.isDown) {
+			this.player.x += this.speed;
+		}
+	}
+}
+
 const game = new Phaser.Game({
 	width: 500,
 	height: 500,
@@ -10,57 +69,3 @@ const game = new Phaser.Game({
 	},
 	scene: [gameScene],
 });
-
-gameScene.init = function () {
-	this.speed = 3;
-};
-
-gameScene.preload = function () {
-	this.load.image('player', 'assets/images/player.png');
-};
-
-gameScene.create = function () {
-	this.player = this.physics.add.sprite(100, 100, 'player');
-	this.player.setScale(3);
-
-	this.upKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
-	this.downKey = this.input.keyboard.addKey(
-		Phaser.Input.Keyboard.KeyCodes.DOWN
-	);
-	this.leftKey = this.input.keyboard.addKey(
-		Phaser.Input.Keyboard.KeyCodes.LEFT
-	);
-	this.rightKey = this.input.keyboard.addKey(
-		Phaser.Input.Keyboard.KeyCodes.RIGHT
-	);
-
-	this.input.on('pointermove', (event) => {
-		const angle =
-			Phaser.Math.RAD_TO_DEG * // converts the radians to degress
-			Phaser.Math.Angle.Between( // calculates the angle in radians
-				this.player.x,
-				this.player.y,
-				event.x,
-				event.y
-			);
-		this.player.setAngle(angle);
-	});
-};
-
-gameScene.update = function () {
-	if (this.upKey.isDown) {
-		this.player.y -= this.speed;
-	}
-
-	if (this.downKey.isDown) {
-		this.player.y += this.speed;
-	}
-
-	if (this.leftKey.isDown) {
-		this.player.x -= this.speed;
-	}
-
-	if (this.rightKey.isDown) {
-		this.player.x += this.speed;
-	}
-};
