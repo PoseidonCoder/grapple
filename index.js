97a5bc14303769554f6ad2772dfe6f69d33cd667
Lukas <computorgenius101@gmail.com>
2021-03-15 18:10:16 -0700

    ✨ basic multiplayer player movements #7

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+
+let players = {};
+
+io.on("connection", (socket) => {
+  Object.keys(players).forEach((id) => {
+    socket.emit("newPlayer", {
+      pos: players[id],
+      id: id,
+    });
+  });
+
+  socket.on("newPlayer", function (player) {
+    players[socket.id] = {
+      x: player.x,
+      y: player.y,
+    };
+
+    io.emit("newPlayer", {
+      pos: players[socket.id],
+      id: socket.id,
+    });
+  });
+
+  socket.on("player", function (player) {
+    players[socket.id].x = player.x;
+    players[socket.id].y = player.y;
+
+    socket.broadcast.emit("players", players);
+  });
+});
+
+app.use(express.static("public"));
+http.listen(8080);
---
+++ public/assets/scripts/game.js
@@ -0,0 +1,151 @@
+class gameScene extends Phaser.Scene {
+	constructor() {
+		super();
+
+		this.speed = 3;
+		this.sprintAcceleration = 2;
+		this.socket = io();
+	}
+
+	preload() {
+		this.load.image('player', 'assets/images/player.png');
+		this.load.image('bullet', 'assets/images/bullet.png');
+	}
+
+	create() {
+		this.start = this.getTime();
+
+		this.players = this.physics.add.group();
+		this.socket.on('newPlayer', (player) => {
+			const newPlayer = this.add.sprite(
+				player.pos.x,
+				player.pos.y,
+				'player'
+			);
+			newPlayer.setScale(3);
+			newPlayer.id = player.id;
+
+			this.players.add(newPlayer);
+		});
+
+		this.socket.on('players', (players) => {
+			Object.keys(players).forEach((id) => {
+				if (id != this.socket.id) {
+					this.players.getChildren().forEach((player) => {
+						if (player.id == id) {
+							player.setPosition(players[id].x, players[id].y);
+						}
+					});
+				}
+			});
+		});
+
+		this.physics.world.setBounds(0, 0, 500, 500);
+		this.cameras.main.setBounds(0, 0, 500, 500);
+
+		this.player = this.physics.add.sprite(100, 100, 'player');
+		this.player.setScale(3);
+		this.player.setCollideWorldBounds(true);
+		this.cameras.main.startFollow(this.player, true);
+
+		this.socket.emit('newPlayer', {
+			x: this.player.x,
+			y: this.player.y,
+		});
+
+		this.bulletGroup = new bulletGroup(this);
+
+		this.upKey = this.input.keyboard.addKey(
+			Phaser.Input.Keyboard.KeyCodes.UP
+		);
+
+		this.wKey = this.input.keyboard.addKey(
+			Phaser.Input.Keyboard.KeyCodes.W
+		);
+
+		this.downKey = this.input.keyboard.addKey(
+			Phaser.Input.Keyboard.KeyCodes.DOWN
+		);
+
+		this.sKey = this.input.keyboard.addKey(
+			Phaser.Input.Keyboard.KeyCodes.S
+		);
+
+		this.leftKey = this.input.keyboard.addKey(
+			Phaser.Input.Keyboard.KeyCodes.LEFT
+		);
+
+		this.aKey = this.input.keyboard.addKey(
+			Phaser.Input.Keyboard.KeyCodes.A
+		);
+
+		this.rightKey = this.input.keyboard.addKey(
+			Phaser.Input.Keyboard.KeyCodes.RIGHT
+		);
+
+		this.dKey = this.input.keyboard.addKey(
+			Phaser.Input.Keyboard.KeyCodes.D
+		);
+
+		this.input.keyboard.on('keydown-SHIFT', (event) => {
+			this.speed += this.sprintAcceleration;
+		});
+
+		this.input.keyboard.on('keyup-SHIFT', (event) => {
+			this.speed -= this.sprintAcceleration;
+		});
+
+		this.input.on('pointermove', (event) => {
+			const angle =
+				Phaser.Math.RAD_TO_DEG * // converts the radians to degress
+				Phaser.Math.Angle.Between(
+					// calculates the angle in radians
+					this.player.x,
+					this.player.y,
+					event.x,
+					event.y
+				);
+			this.player.setAngle(angle);
+		});
+	}
+
+	update() {
+		if (this.upKey.isDown || this.wKey.isDown) {
+			this.player.y -= this.speed;
+			this.cameras.main.scrollY -= this.speed;
+		}
+
+		if (this.downKey.isDown || this.sKey.isDown) {
+			this.player.y += this.speed;
+			this.cameras.main.scrollY += this.speed;
+		}
+
+		if (this.leftKey.isDown || this.aKey.isDown) {
+			this.player.x -= this.speed;
+			this.cameras.main.scrollX -= this.speed;
+		}
+
+		if (this.rightKey.isDown || this.dKey.isDown) {
+			this.player.x += this.speed;
+			this.cameras.main.scrollX += this.speed;
+		}
+
+		if (this.input.activePointer.isDown && this.showDelta() > 100) {
+			this.bulletGroup.fire(this.player.x, this.player.y - 20);
+			this.start = this.getTime();
+		}
+
+		this.socket.emit('player', {
+			x: this.player.x,
+			y: this.player.y,
+		});
+	}
+
+	getTime() {
+		return new Date().getTime();
+	}
+
+	showDelta() {
+		return this.getTime() - this.start;
+	}
+}
