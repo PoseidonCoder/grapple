5886c385a83d3b5a74ceb8c9bba052baf6a05374
Lukas <computorgenius101@gmail.com>
2021-03-22 15:22:59 -0700

    ✨ added loading bar

--- public/assets/scripts/scenes/game.js
+++ public/assets/scripts/scenes/game.js
@@ -25,6 +25,38 @@ class gameScene extends Phaser.Scene {
 
 		this.load.audio('pew', 'assets/sounds/shoot.mp3');
 		this.load.audio('music', 'assets/sounds/music.mp3');
+
+		const progressBar = this.add.graphics();
+		const progressBox = this.add.graphics();
+		progressBox.fillStyle(0x222222, 0.8);
+		progressBox.fillRect(
+			globals.mapWidth / 2.7,
+			globals.mapHeight / 2.7,
+			320,
+			50
+		);
+
+		this.load.on('progress', (value) => {
+			console.log(value);
+			progressBar.clear();
+			progressBar.fillStyle(0xffffff, 1);
+			progressBar.fillRect(
+				globals.mapWidth / 2.7 + 10,
+				globals.mapHeight / 2.7 + 10,
+				300 * value,
+				30
+			);
+		});
+
+		this.load.on('fileprogress', (file) => {
+			console.log(file.src);
+		});
+
+		this.load.on('complete', () => {
+			console.log('complete');
+			progressBar.destroy();
+			progressBox.destroy();
+		});
 	}
 
 	create() {
