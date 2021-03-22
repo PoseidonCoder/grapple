cb4ad40d54ab6eb0f4af1b62c521a9bebf109d61
Lukas <computorgenius101@gmail.com>
2021-03-21 19:56:38 -0700

    ⚡️ minor build performance improvements

--- public/webpack.config.js
+++ public/webpack.config.js
@@ -1,11 +1,24 @@
 const path = require('path');
+const MeasureSpeed = require('speed-measure-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-	mode: 'production',
+	mode: 'development',
 	cache: true,
-	entry: './assets/scripts/main.js',
+	entry: {
+		main: './assets/scripts/main.js'
+	},
 	output: {
 		path: path.resolve(__dirname, 'assets/dist'),
 		filename: 'main.js',
 	},
+	optimization: {
+		minimizer: [
+			new UglifyJsPlugin({
+				parallel: true,
+				cache: true,
+			}),
+		],
+	},
+	plugins: [new MeasureSpeed()],
 };
