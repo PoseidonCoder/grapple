957b13d47b2b294e3dd92272f17fd212a38762f4
Lukas <computorgenius101@gmail.com>
2021-03-24 18:53:08 -0700

    ✨ store google ids

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
+const redis = require('redis');
+const client = redis.createClient();
+
 const { google } = require('googleapis');
 
 if (process.env.NODE_ENV !== 'production') {
@@ -15,6 +18,25 @@ const oauth = new google.auth.OAuth2(
 	process.env.CLIENT_SECRET,
 	'http://localhost:8080/auth'
 );
+
+app.get('/create', (req, res) => {
+	const url = oauth.generateAuthUrl({
+		scope: 'https://www.googleapis.com/auth/userinfo.email',
+	});
+
+	res.redirect(url);
+});
+
+app.get('/auth', async (req, res) => {
+	const { tokens } = await oauth.getToken(req.query.code);
+	oauth.setCredentials(tokens);
+
+	client.hmset(tokens.id_token, {
+		name: '',
+	});
+
+	res.redirect('/');
+});
 
 let players = {};
 const frameRate = 30;
