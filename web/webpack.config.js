d853c27cbe83c0fc31bdd2d72307208a78086d28
Lukas <computorgenius101@gmail.com>
2021-04-03 17:16:52 -0700

    ⬆️ upgrade uglifyjs version

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -2,26 +2,28 @@ const path = require('path');
 const MeasureSpeed = require('speed-measure-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
-module.exports = {
-	mode: 'development',
-	cache: true,
-	entry: {
-		main: './assets/scripts/main.js'
-	},
-	output: {
-		path: path.resolve(__dirname, 'assets/dist'),
-		filename: 'main.js',
-	},
-	optimization: {
-		minimizer: [
-			new UglifyJsPlugin({
-				parallel: true,
-				cache: true,
-			}),
-		],
-	},
-	experiments: {
-		topLevelAwait: true,
-	},
-	plugins: [new MeasureSpeed()],
+module.exports = (env) => {
+	return {
+		mode: env.production ? 'production' : 'development',
+		cache: true,
+		entry: {
+			main: './assets/scripts/main.js',
+		},
+		output: {
+			path: path.resolve(__dirname, 'assets/dist'),
+			filename: 'main.js',
+		},
+		optimization: {
+			minimizer: [
+				new UglifyJsPlugin({
+					parallel: true,
+					cache: true,
+				}),
+			],
+		},
+		experiments: {
+			topLevelAwait: true,
+		},
+		plugins: [new MeasureSpeed()],
+	};
 };
