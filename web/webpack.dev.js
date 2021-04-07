ec45e6d27b43c32f1995de4496c2d3a8bf7742a3
Lukas <computorgenius101@gmail.com>
2021-04-07 13:57:18 -0700

    🔨 added dev server

--- web/webpack.dev.js
+++ web/webpack.dev.js
@@ -1,10 +1,16 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
+const path = require('path');
 const { DefinePlugin } = require('webpack');
 
 module.exports = merge(common, {
 	mode: 'development',
+	devServer: {
+		contentBase: path.join(__dirname, 'assets/dist'),
+		compress: true,
+		port: 8000,
+	},
 	plugins: [
 		new DefinePlugin({
 			PRODUCTION: JSON.stringify(false),
